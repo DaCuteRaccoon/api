dd2e6c5ddc61cf4ac34c05d808fe37d78c83ddbe
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-01-27 18:09:02 -0800

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,11 @@
+// Don't have time for this right now, gotta go somewhere
 
+/*
+https://www.google.com/search?q=dictionary+api
+https://api.github.com/
+http://mikemcl.github.io/decimal.js/
+https://en.wikipedia.org/wiki/Trefoil_knot
+https://upload.wikimedia.org/wikipedia/commons/4/46/Trefoil_knot_arb.png
+https://www.google.com/search?q=scrabble+word+api
+https://en.gravatar.com/
+*/
